dde60e095305249e75b495868b4a35ea8be99ea3
Bikey Bhujel <bicky.bhujel@gmail.com>
2018-08-03 10:31:22 +0545

    added  mail sending feature with swift mail

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,7 @@
+<h3>You have a new contact via Contact Form</h3>
+
+<div>
+	{{ $bodyMessage }}
+</div>
+
+<p>Sent Via {{ $email }}</p>
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -7,20 +7,21 @@
 		<div class="col-md-6 offset-md-3">
 			<h1 class="text-center">Contact Me</h1>
 			<hr>
-			<form>
+			<form method="post" action="{{  url('contact') }}">
+				{{csrf_field()}}
 				<div class="form-group">
 					<label for="email" class="font-weight-bold">Email: </label>
-					<input id="email" name="email" class="form-control">
+					<input id="email" name="email" class="form-control" placeholder="Type ur email here">
 				</div><!-- end of form group-->
 
 				<div class="form-group">
 					<label for="email" class="font-weight-bold">Subject: </label>
-					<input id="subject" name="subject" class="form-control">
+					<input id="subject" name="subject" class="form-control" placeholder="Type your subject here">
 				</div><!-- end of form group-->
 
 				<div class="form-group">
 					<label for="message" class="font-weight-bold">Message: </label>
-					<textarea id="message" name="message" class="form-control">Type your message here...</textarea>
+					<textarea id="message" name="message" class="form-control" placeholder="Type your message here"></textarea>
 				</div><!-- end of form group-->
 
 				<input type="submit" value="send message" class="btn btn-success text-center">
