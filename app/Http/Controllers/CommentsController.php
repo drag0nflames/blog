f8910c0a10dcd19fd7e9a3cf38e258d2c8289c81
Bikey Bhujel <bicky.bhujel@gmail.com>
2018-08-04 15:59:51 +0545

    added controller for comment

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Comment;
+use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Session;
 
-class CommentController extends Controller
+class CommentsController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -32,9 +36,38 @@ class CommentController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $post_id)
     {
-        //
+		$post = Post::find($post_id);
+
+		//validate the data
+		$validator = Validator::make($request->all(), [
+			'name' => 'required|max:255',
+			'email' => 'required|email|max:255',
+			'comment' => 'required|min:5|max:2000'
+
+		]);
+
+		if ($validator->fails()) {
+			return redirect(route('blog.single', [$post->slug]))
+				->withErrors($validator)
+				->withInput();
+		}
+
+		$post = Post::find($post_id);
+
+		$comment = new Comment();
+		$comment->name = $request->name;
+		$comment->email = $request->email;
+		$comment->comment = $request->comment;
+		$comment->approved = true;
+		$comment->post()->associate($post);
+
+		$comment->save();
+
+		Session::flash('success','The comment was successfully posted.');
+
+		return redirect()->route('blog.single', [$post->slug]);
     }
 
     /**
