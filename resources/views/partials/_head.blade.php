e05071924a742b951be5d86e92592ee01fc02031
Bikey Bhujel <bicky.bhujel@gmail.com>
2018-08-06 11:05:31 +0545

    added raleway google font

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,15 +1,21 @@
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css"
-          integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-    <link type="text/css" rel="stylesheet" href="{{ URL::asset('css/blog.css') }}">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css"
+		  integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+	<link type="text/css" rel="stylesheet" href="{{ URL::asset('css/blog.css') }}">
+	<link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
 
-    @yield('stylesheets')
+	<!-- Font awesome-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+		  integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 
 
-    <title>Laravel Blog @yield('title')</title><!-- change this title for each page-->
+	@yield('stylesheets')
+
+
+	<title>Laravel Blog @yield('title')</title><!-- change this title for each page-->
 </head>
