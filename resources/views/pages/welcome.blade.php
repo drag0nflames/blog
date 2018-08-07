5bab83601f05026dbbb2e00fb4f930adaffdb2eb
Bikey Bhujel <bicky.bhujel@gmail.com>
2018-08-07 13:27:52 +0545

    added carousel

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -5,6 +5,49 @@
 @section('content')
 	<div class="row">
 		<div class="col-md-12">
+			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+					<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+					<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				</ol>
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						<img class="d-block w-100" src="{{ URL::asset('img/Carousel_Large_1.jpg') }}?auto=yes&bg=777&fg=555&text=First slide" alt="First slide">
+						<div class="carousel-caption d-none d-md-block">
+							<h5>Twitter Bootstrap 4</h5>
+							<p>Create your own website using twitter bootstrap 4</p>
+						</div>
+					</div>
+					<div class="carousel-item">
+						<img class="d-block w-100" src="{{ URL::asset('img/Carousel_Large_2.jpg') }}?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
+						<div class="carousel-caption d-none d-md-block">
+							<h5>Javascript</h5>
+							<p>Add effects to your website with Javascript</p>
+						</div>
+					</div>
+					<div class="carousel-item">
+						<img class="d-block w-100" src="{{ URL::asset('img/Carousel_Large_3.jpg') }}?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
+						<div class="carousel-caption d-none d-md-block">
+							<h1>Laravel</h1>
+							<p>Laravel is a free, open-source PHP web framework intended for the development of web applications following MVC architectural pattern and based on Symfony.</p>
+						</div>
+					</div>
+				</div>
+				<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
+			</div>
+		</div><!-- end of col-md-12-->
+	</div><!-- end of row-->
+
+	<div class="row">
+		<div class="col-md-12">
 			@if(session('status'))
 				<div class="alert alert-success">
 					{{ session('status') }}
