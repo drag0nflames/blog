a28e88576b31cbbb86e86657bc49d223641265bc
Bikey Bhujel <bicky.bhujel@gmail.com>
2018-08-07 13:26:56 +0545

    added styling

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -1,23 +1,80 @@
 @extends('main')
-
 <?php $titleTag = htmlspecialchars($post->title); ?>
 @section('title', " | $titleTag")
 
 @section('content')
 	<div class="row">
-		<div class="col-md-8 offset-md-2">
-			<h1 class="text-center">{{ $post->title }}</h1>
-			<hr>
-			<p clas="lead">{{ $post->body }}</p>
-			<hr>
-			<p> Posted In: {{ $post->category->name }}</p>
-		</div><!--end of col-md-8-->
-	</div><!-- end of row-->
+		<div class="col-md-8">
+			<article class="post-detail post-item">
+				<div class="post-item-body padding-10">
+					<h1>{{ $post->title }}</h1>
+					<div class="post-meta no-border">
+						<ul class="post-meta-group">
+							<li><i class="fa fa-user"></i><a href="#"> Admin</a></li>
+							<li><i class="fa fa-clock"></i>
+								<time>{{ date('M j, Y h:iA', strtotime($post->created_at))}}</time>
+							</li>
+							<li><i class="fa fa-tags"></i>
+								@foreach($post->tags as $tag)
+									<span class="badge badge-secondary">{{ $tag->name }}</span>
+								@endforeach
+							</li>
+							<li><i class="fa fa-comments"></i><a href="#comment_id"> {{ $post->comments->count() }} Comments</a></li>
+						</ul>
+					</div>
+					<p clas="lead">{{ $post->body }}</p>
+					<p> Posted In: {{ $post->category->name }}</p>
+				</div>
+			</article><!-- end of article-->
 
-	<div class="row comment-section">
-		<div class="col-md-8 offset-md-2">
+
+			<article class="post-author padding-10">
+				<div class="media">
+					<div class="media-left">
+						<a href="#">
+							{{--TODO add image column in user table and access it from here dynamically--}}
+							<img alt="Author 1" src="{{ URL::asset('img/author.jpg') }}" class="media-object">
+						</a>
+					</div>
+					<div class="media-body">
+						<h2 class="font-weight-bold">Author:</h2>
+						<h4 class="media-heading"><a href="#">Masaru Edo</a></h4>
+						<div class="post-author-count">
+							<a href="#">
+								<i class="fa fa-clone"></i>
+								90 posts
+							</a>
+						</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad aut sunt cum, mollitia
+							excepturi neque sint magnam minus aliquam, voluptatem, labore quis praesentium eum quae
+							dolorum temporibus consequuntur! Non.</p>
+					</div>
+				</div><!-- end of media-->
+			</article><!-- end of article-->
+
+			<article class="post-comments" id="comment_id">
+				<h3><i class="fa fa-comments"></i> {{$post->comments->count()}} Comments</h3>
+				<div class="comment-body padding-10">
+					<ul class="comments-list">
+						@foreach($post->comments as $comment)
+							<li class="comment-item">
+								<div class="comment-heading clearfix">
+									<div class="comment-author-meta">
+										<h4>{{$comment->name}}</h4>
+										<small>{{ date('M j, Y h:iA', strtotime($comment->created_at))}}</small>
+									</div>
+								</div>
+								<div class="comment-content">
+									<p>{{$comment->comment}}</p>
+								</div>
+							</li>
+						@endforeach
+					</ul>
+				</div>
+			</article>
+
 			<div class="card">
-				<h5 class="card-header text-center">Comment Form</h5>
+				<h5 class="card-header text-center font-weight-bold">Comment Form</h5>
 				<div class="card-body">
 					<form method="post" action="{{ route('comments.store', [$post->id]) }}">
 						{{csrf_field()}}
@@ -47,6 +104,6 @@
 					</form><!-- end of form-->
 				</div>
 			</div>
-		</div><!-- end of col-md-8-->
+		</div><!--end of col-md-8-->
 	</div><!-- end of row-->
 @endsection
