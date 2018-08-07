0ca61c192bd76d8614418107ebb35715e62803c7
Bikey Bhujel <bicky.bhujel@gmail.com>
2018-08-07 12:45:58 +0545

    removed table name

--- app/Comment.php
+++ app/Comment.php
@@ -9,6 +9,8 @@ class Comment extends Model
 	/**
 	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
 	 */
+	protected $table = 'comment_post';
+
 	public function post(){
 		return $this->belongsTo('App\Post');
 	}
