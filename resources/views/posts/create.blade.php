81389f20dd14ea5fc9a571016bd60fbe6971975d
Bikey Bhujel <bicky.bhujel@gmail.com>
2018-08-11 09:13:13 +0545

    added tiny mce cdn

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,16 +3,23 @@
 @section('stylesheets')
 	<link type="text/css" rel="stylesheet" href="{{ URL::asset('css/parsley.css') }}">
 	<link type="text/css" rel="stylesheet" href="{{ URL::asset('css/select2.css') }}">
+	<script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+	<script>
+        tinymce.init({
+            selector: 'textarea',
+            plugins: "link code",
+        });
+	</script>
 @endsection
 
 @section('title', '| Create New Post')
 
 @section('content')
 	<div class="row">
-		<div class="col-md-6 offset-md-3">
+		<div class="col-md-8 offset-md-2">
 			<h1 class="text-center">Create New Post</h1>
 			<hr>
-			<form method="POST" action={{ route('posts.store') }} data-parsley-validate>
+			<form method="POST" action={{ route('posts.store') }} data-parsley-validate enctype="multipart/form-data">
 				<div class="form-group">
 					<label for="title" class="font-weight-bold">Title: </label>
 					<input type="text" id="title" name="title" class="form-control" value="{{ old('title') }}"
@@ -31,7 +38,7 @@
 
 				<div class="form-group">
 					<label for="category" class="font-weight-bold">Category: </label>
-					<select class="form-control" id="category_id" name="category_id">
+					<select class="form-control" id="category_id" name="category_id" required>
 						@foreach($categories as $category)
 							<option value="{{ $category->id }}">{{ $category->name }}</option>
 						@endforeach
@@ -40,7 +47,7 @@
 
 				<div class="form-group">
 					<label for="tags" class="font-weight-bold">Tags: </label>
-					<select class="form-control select2-selection--multiple" name="tags[]" multiple="multiple">
+					<select class="form-control select2-selection--multiple" name="tags[]" multiple="multiple" required>
 						@foreach($tags as $tag)
 							<option value="{{ $tag->id }}">{{ $tag->name }}</option>
 						@endforeach
@@ -48,8 +55,23 @@
 				</div><!--end of form-group-->
 
 				<div class="form-group">
+					{{--<label for="featured_image" class="font-weight-bold">Featured Image: </label>--}}
+					{{--<input type="file" name="featured_image" class="form-control">--}}
+					<label for="featured_image" class="font-weight-bold">Featured Image: </label>
+					<div class="input-group">
+            			<span class="input-group-btn">
+               				 <span class="btn btn-primary btn-file">
+                   		 		Browse… <input type="file" name="featured_image" id="featured_image">
+                			</span>
+						</span>
+						<input type="text" class="form-control" readonly>
+					</div>
+					<img id='img-upload'/>
+				</div>
+
+				<div class="form-group">
 					<label for="body" class="font-weight-bold">Body: </label>
-					<textarea rows="6" id="body" name="body" class="form-control" required
+					<textarea rows="9" id="body" name="body" class="form-control" required
 							  placeholder=" Enter The Body Of Blog">{{ old('body') }}</textarea>
 				</div><!--end of form-group-->
 
@@ -65,6 +87,43 @@
 	<script type="text/javascript" src="{{ URL::asset('js/parsley.min.js') }}"></script>
 	<script type="text/javascript" src="{{ URL::asset('js/select2.full.js') }}"></script>
 	<script type="text/javascript">
-		$('.select2-selection--multiple').select2();
+        $(document).ready( function() {
+            $(document).on('change', '.btn-file :file', function() {
+                var input = $(this),
+                    label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+                input.trigger('fileselect', [label]);
+            });
+
+            $('.btn-file :file').on('fileselect', function(event, label) {
+
+                var input = $(this).parents('.input-group').find(':text'),
+                    log = label;
+
+                if( input.length ) {
+                    input.val(log);
+                } else {
+                    if( log ) alert(log);
+                }
+
+            });
+            function readURL(input) {
+                if (input.files && input.files[0]) {
+                    var reader = new FileReader();
+
+                    reader.onload = function (e) {
+                        $('#img-upload').attr('src', e.target.result);
+                    }
+
+                    reader.readAsDataURL(input.files[0]);
+                }
+            }
+
+            $("#featured_image").change(function(){
+                readURL(this);
+            });
+        });
+	</script>
+	<script type="text/javascript">
+        $('.select2-selection--multiple').select2();
 	</script>
 @endsection
