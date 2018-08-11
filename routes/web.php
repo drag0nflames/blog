ec095ec319df4160bbb8b155fa2c1b49d280d670
Bikey Bhujel <bicky.bhujel@gmail.com>
2018-08-11 09:21:34 +0545

    added routes for editing and deleting comment

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,9 @@
 
 		//comment controller
 		Route::post('comments/{post_id}', 'CommentsController@store')->name('comments.store');
-
+        Route::get('comments/{id}/edit', 'CommentsController@edit')->name('comments.edit');
+		Route::put('comments/{id}', 'CommentsController@update')->name('comments.update');
+		Route::delete('comments/{id}', 'CommentsController@destroy')->name('comments.destroy');
 
 		Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
 
